ec7f5d4dc66aef79fb97a6ec3d6a198b2c67fe20
Meet200 <58128162+Meet200@users.noreply.github.com>
2021-06-21 13:00:38 +0530

    Add files via upload

---
+++ Document_view/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Document_view.rc
+//
+#define IDR_MAINFRAME                   101
+#define ID_FILE_EXIT                    40001
+#define ID_FILE_OPEN                    40002
+#define ID_FILE_PRINTSETUP              40003
+#define ID_FILE_RECENTFILE              40004
+#define ID_FILE_EXIT40005               40005
+#define ID_FILE_NEW                     40006
+#define ID_FILE_PRINT_SETUP             40007
+#define ID_FILE_MRU_FILE1               40008
+#define IID_APP_EXIT                    40009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
