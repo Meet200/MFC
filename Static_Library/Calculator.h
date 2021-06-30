e0442b72f74a3d3db56de76ae4d6bfbdd9a78696
Meet200 <58128162+Meet200@users.noreply.github.com>
2021-06-30 10:05:26 +0530

    Add files via upload

---
+++ Static_Library/Calculator.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _CALCULATOR_H_
+#define _CALCULATOR_H_
+double Min(const double* Numbers, const int Count);
+double Max(const double* Numbers, const int Count);
+double Sum(const double* Numbers, const int Count);
+double Average(const double* Numbers, const int Count);
+long GreatestCommonDivisor(long Nbr1, long Nbr2);
+#endif // _CALCULATOR_H_
